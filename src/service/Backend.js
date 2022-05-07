ab5943e26159d500460bdd7e4a5b259475a321cd
AracelySC <84749072+AracelySC@users.noreply.github.com>
2022-05-07 03:55:54 -0600

    Update Backend.js

--- src/service/Backend.js
+++ src/service/Backend.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://euclides.azurewebsites.net';
+// const BASE_URL = 'http://localhost:3000';
 const backend = {
     getAprendizaje: async (data) => {
 
